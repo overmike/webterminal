620af8b1c572d770ec25c43f0f1bac015f952212
Mike Lee <mike.lee@databiology.com>
2018-11-07 04:45:16 +0800

    config root command

--- cmd/root.go
+++ cmd/root.go
@@ -50,7 +50,7 @@ func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.test.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.webterminal.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
@@ -70,9 +70,9 @@ func initConfig() {
 			os.Exit(1)
 		}
 
-		// Search config in home directory with name ".test" (without extension).
+		// Search config in home directory with name ".webterminal" (without extension).
 		viper.AddConfigPath(home)
-		viper.SetConfigName(".test")
+		viper.SetConfigName(".webterminal")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
